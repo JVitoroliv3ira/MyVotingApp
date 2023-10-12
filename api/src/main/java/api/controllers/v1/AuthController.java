9cf333e29042d2bd9dd2be6662061c94ff0138ae
Joao Vitor <jvitoroliveira0711@gmail.com>
2023-10-12 15:01:19 -0300

    feature(backend): Implementa versão inicial do endpoint de cadastro de usuarios

--- api/src/main/java/api/controllers/v1/AuthController.java
+++ api/src/main/java/api/controllers/v1/AuthController.java
@@ -1,13 +1,32 @@
 package api.controllers.v1;
 
+import api.dtos.requests.UserRegistrationRequestDTO;
+import api.dtos.responses.ResponseDTO;
+import api.models.User;
 import api.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/v1/auth")
 @RestController
 public class AuthController {
     private final UserService userService;
+
+    @PostMapping(path = "/register")
+    public ResponseEntity<ResponseDTO<User>> register(@Valid @RequestBody UserRegistrationRequestDTO request) {
+        this.userService.validateEmailUniqueness(request.getEmail());
+        User createdUser = this.userService.create(request.convert());
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDTO<>(createdUser, null));
+    }
 }
