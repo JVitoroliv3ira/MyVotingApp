64b009db8e0452d85846b66108c0f9c9ab2038b8
Joao Vitor <jvitoroliveira0711@gmail.com>
2023-10-12 12:24:04 -0300

    feature(backend): Adiciona metodo de validacao que o email informado é único

--- api/src/main/java/api/services/UserService.java
+++ api/src/main/java/api/services/UserService.java
@@ -1,5 +1,6 @@
 package api.services;
 
+import api.exceptions.UnprocessableEntityException;
 import api.models.User;
 import api.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,14 +9,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class UserService {
+    private static final String EMAIL_ALREADY_IN_USE_ERROR_MESSAGE = "O email informado já está sendo utilizado por outro usuário.";
     private final UserRepository repository;
-    
+
     public User create(User user) {
         user.setId(null);
         return this.repository.save(user);
     }
 
-    public Boolean isEmailAlreadyInUse(String email) {
+    public void validateEmailUniqueness(String email) throws UnprocessableEntityException {
+        if (Boolean.TRUE.equals(this.isEmailAlreadyInUse(email))) {
+            throw new UnprocessableEntityException(EMAIL_ALREADY_IN_USE_ERROR_MESSAGE);
+        }
+    }
+
+    private Boolean isEmailAlreadyInUse(String email) {
         return this.repository.existsByEmail(email);
     }
 }
