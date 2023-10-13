b9d0d1815d52e64b37c2dc7cdf7d2a45a4d61aaa
Joao Vitor <jvitoroliveira0711@gmail.com>
2023-10-12 22:09:15 -0300

    feature(backend): Adiciona novos metodos no UserService

--- api/src/main/java/api/services/UserService.java
+++ api/src/main/java/api/services/UserService.java
@@ -1,15 +1,22 @@
 package api.services;
 
+import api.dtos.DetailsDTO;
+import api.exceptions.BadRequestException;
 import api.exceptions.UnprocessableEntityException;
 import api.models.User;
 import api.repositories.UserRepository;
+import api.utils.CryptographyUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     private static final String EMAIL_ALREADY_IN_USE_ERROR_MESSAGE = "O email informado já está sendo utilizado por outro usuário.";
+    private static final String USER_NOT_FOUND_ERROR_MESSAGE = "Usuário não encontrado na base de dados.";
+    private static final String USER_PASSWORD_INCORRECT_ERROR_MESSAGE = "A senha ou o endereço de e-mail está incorreto.";
     private final UserRepository repository;
 
     public User create(User user) {
@@ -25,5 +32,19 @@ public class UserService {
 
     private Boolean isEmailAlreadyInUse(String email) {
         return this.repository.existsByEmail(email);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws BadRequestException {
+        User user = this.repository
+                .findByEmail(email)
+                .orElseThrow(() -> new BadRequestException(USER_NOT_FOUND_ERROR_MESSAGE));
+        return new DetailsDTO(user);
+    }
+
+    public void validatePasswordMatches(String rawPassword, String encodedPassword) {
+        if (Boolean.FALSE.equals(CryptographyUtil.valuesMatches(rawPassword, encodedPassword))) {
+            throw new BadRequestException(USER_PASSWORD_INCORRECT_ERROR_MESSAGE);
+        }
     }
 }
