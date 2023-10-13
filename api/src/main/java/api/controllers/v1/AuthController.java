b1d3417f1cf8362bb0fbac578f4bb1766bc74232
Joao Vitor <jvitoroliveira0711@gmail.com>
2023-10-12 22:13:07 -0300

    feature(backend): Adiciona endpoint de autenticacao na aplicacao

--- api/src/main/java/api/controllers/v1/AuthController.java
+++ api/src/main/java/api/controllers/v1/AuthController.java
@@ -1,12 +1,16 @@
 package api.controllers.v1;
 
+import api.dtos.requests.UserAuthenticationRequestDTO;
 import api.dtos.requests.UserRegistrationRequestDTO;
+import api.dtos.responses.AuthenticatedUserResponseDTO;
 import api.dtos.responses.ResponseDTO;
 import api.models.User;
+import api.services.JwtService;
 import api.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +23,7 @@ import javax.validation.Valid;
 @RestController
 public class AuthController {
     private final UserService userService;
+    private final JwtService jwtService;
 
     @PostMapping(path = "/register")
     public ResponseEntity<ResponseDTO<User>> register(@Valid @RequestBody UserRegistrationRequestDTO request) {
@@ -28,5 +33,15 @@ public class AuthController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(new ResponseDTO<>(createdUser, null));
+    }
+
+    @PostMapping(path = "/login")
+    public ResponseEntity<ResponseDTO<AuthenticatedUserResponseDTO>> login(@Valid @RequestBody UserAuthenticationRequestDTO request) {
+        UserDetails details = this.userService.loadUserByUsername(request.getEmail());
+        this.userService.validatePasswordMatches(request.getPassword(), details.getPassword());
+        String token = this.jwtService.generateToken(details);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(new ResponseDTO<>(new AuthenticatedUserResponseDTO(details, token), null));
     }
 }
