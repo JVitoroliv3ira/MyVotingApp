eb4b27f54d926a112c49d1403ad8dcf7cd71f05c
jvitor07 <jvitoroliveira0711@gmail.com>
2023-10-15 16:45:40 -0300

    feature(backend): Adiciona anotacao do profile ativo

--- api/src/test/java/api/ApiApplicationTests.java
+++ api/src/test/java/api/ApiApplicationTests.java
@@ -2,12 +2,14 @@ package api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles(profiles = {"h2"})
 @SpringBootTest
 class ApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
